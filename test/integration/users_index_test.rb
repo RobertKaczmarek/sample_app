e383daf862e2d969c8bc65e35e9e51c2f7393c1e
Robert Kaczmarek <kaczmarek.robert95@gmail.com>
2017-01-15 02:51:49 -0800

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,6 +4,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:robert)
     @non_admin = users(:szymon)
+    @unactivated_user = users(:maciej)
   end
 
   test "index as admin including pagination and delete links" do
@@ -13,9 +14,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination', count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
       end
     end
     assert_difference 'User.count', -1 do
@@ -27,5 +32,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "should show only activated users" do
+    log_in_as(@admin)
+    get user_path(@unactivated_user)
+    assert_redirected_to root_url
   end
 end
